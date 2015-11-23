f97bda8a03057c4dba3ec996bcd025eebf601806
Sashko Stubailo <sashko@stubailo.com>
2015-11-22 22:48:24 -0800

    Add non-working second test

--- method-tests.js
+++ method-tests.js
@@ -1,8 +1,17 @@
-const methodName = 'method';
-
-const method = new Method({
-  name: methodName,
+const plainMethod = new Method({
+  name: 'plainMethod',
   schema: new SimpleSchema({}),
+  run() {
+    return 'result';
+  }
+});
+
+const methodWithArgs = new Method({
+  name: 'methodWithArgs',
+  schema: new SimpleSchema({
+    int: { type: Number },
+    string: { type: String },
+  }),
   run() {
     return 'result';
   }
@@ -10,10 +19,23 @@ const method = new Method({
 
 describe('mdg:method', () => {
   it('defines a method that can be called', (done) => {
-    method.call({}, (error, result) => {
+    plainMethod.call({}, (error, result) => {
       assert.equal(result, 'result');
 
-      Meteor.call(methodName, {}, (error, result) => {
+      Meteor.call(plainMethod.name, {}, (error, result) => {
+        assert.equal(result, 'result');
+        done();
+      });
+    });
+  });
+
+  it('checks schema', (done) => {
+    methodWithArgs.call({}, (error, result) => {
+      // Never runs
+      console.log(error, result);
+      assert.equal(result, 'result');
+
+      Meteor.call(methodWithArgs.name, {}, (error, result) => {
         assert.equal(result, 'result');
         done();
       });
