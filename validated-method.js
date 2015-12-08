4d16211bce5555d44f0f860a63ba40b43e609e64
okland <omri.klinger@gmail.com>
2015-12-08 19:16:42 +0200

    Update validated-method.js
    Allow support for more than one parameter being used by Meteor.method -
    For example Meteor.call('updateForm', FORM_ID, FORM_MODIFIER) ....

--- validated-method.js
+++ validated-method.js
@@ -24,21 +24,32 @@ ValidatedMethod = class ValidatedMethod {
 
     const method = this;
     this.connection.methods({
-      [name](args) {
+      [name]() {
         // Silence audit-argument-checks since arguments are always checked when using this package
-        check(args, Match.Any);
+        check(arguments, Match.Any);
         const methodInvocation = this;
-        return method._execute(methodInvocation, args);
+        return method._execute(methodInvocation, arguments);
       }
     });
   }
 
-  call(args, callback) {
+  call() {
+    var args = arguments;
     // Accept calling with just a callback
-    if (_.isFunction(args)) {
+    if (args.length == 1 && _.isFunction(args[0])) {
       callback = args;
-      args = {};
+      args = [];
+    } else {
+      // if it's a function, the last argument is the result callback,
+      // not a parameter to the remote method.
+      if (args.length && typeof args[args.length - 1] === "function") {
+        var callback = args.pop();
+      }
     }
+    
+    var args = arguments;
+    
+    return this.apply(name, args, callback);
 
     const options = {
       // Make it possible to get the ID of an inserted item
@@ -52,7 +63,7 @@ ValidatedMethod = class ValidatedMethod {
     };
 
     try {
-      return this.connection.apply(this.name, [args], options, callback);
+      return this.connection.apply(this.name, args, options, callback);
     } catch (err) {
       // Get errors from the stub in the same way as from the server-side method
       callback(err);
@@ -60,13 +71,13 @@ ValidatedMethod = class ValidatedMethod {
   }
 
   _execute(methodInvocation, args) {
-    const validateResult = this.validate.bind(methodInvocation)(args);
+    const validateResult = this.validate.apply(methodInvocation, args);
 
     if (typeof validateResult !== 'undefined') {
       throw new Error(`Returning from validate doesn't do anything; \
 perhaps you meant to throw an error?`);
     }
 
-    return this.run.bind(methodInvocation)(args);
+    return this.run.apply(methodInvocation, args);
   }
 };
